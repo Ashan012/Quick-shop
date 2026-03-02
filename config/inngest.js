d856cf7af60b64d5c07b64d1853d41080cfef2e2
ashan012 <khan89891918@outlook.com>
2026-03-02 07:00:27 +0500

    Add Inngest API functions for user creation, updation, and deletion

--- config/inngest.js
+++ config/inngest.js
@@ -1,4 +1,63 @@
 import { Inngest } from "inngest";
+import connectToDB from "./db";
+import User from "@/models/User";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "quickcart-next" });
+
+export const syncUserCreation = inngest.createFunction(
+  {
+    id: "sync-user-form-clerk",
+  },
+
+  { event: "clerk/user.created" },
+
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+
+    const userData = {
+      _id: id,
+      email: email_addresses[0].email_address,
+      name: first_name + " " + last_name,
+      imageUrl: image_url,
+    };
+    await connectToDB();
+    await User.create(userData);
+  },
+);
+
+export const syncUserUpdation = inngest.createFunction(
+  {
+    id: "update-user-form-clerk",
+  },
+
+  { event: "clerk/user.updated" },
+
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+
+    const userData = {
+      _id: id,
+      email: email_addresses[0].email_address,
+      name: first_name + " " + last_name,
+      imageUrl: image_url,
+    };
+    await connectToDB();
+    await User.findByIdAndDelete(id, userData);
+  },
+);
+
+export const syncUserDeletion = inngest.createFunction(
+  { id: "delete-user-with-clerk" },
+
+  { event: "clerk/user.deleted" },
+
+  async ({ event }) => {
+    const { id } = event.data;
+
+    await connectToDB();
+    await User.findByIdAndDelete(id);
+  },
+);
