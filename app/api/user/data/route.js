740f152094f518002c24e01f1aa14182d4e1dabf
ashan012 <khan89891918@outlook.com>
2026-03-03 07:15:32 +0500

    Fix getAuth usage to correctly retrieve userId in GET request

--- app/api/user/data/route.js
+++ app/api/user/data/route.js
@@ -5,7 +5,7 @@ import { NextResponse } from "next/server";
 
 export async function GET(req) {
   try {
-    const { userId } = getAuth;
+    const { userId } = getAuth(req);
 
     await connectToDB();
 
